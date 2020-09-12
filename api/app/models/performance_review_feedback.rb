6e9e334011b7d540a6a56d6c6633c85b1df76c32
Edmond <Edmond.Akpan@gmail.com>
2020-09-12 21:18:58 +0900

    removes master.key from git tracking and adds helper method for logical pending status

--- api/app/models/performance_review_feedback.rb
+++ api/app/models/performance_review_feedback.rb
@@ -1,4 +1,10 @@
 class PerformanceReviewFeedback < ApplicationRecord
   belongs_to :performance_review
   belongs_to :employee
+
+  # convenience method to check pending status of a required performance review feedback
+  def pending?
+    # nil content field used for logically pending review status
+    content.nil?
+  end
 end
