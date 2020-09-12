a3d9b08d6f965ee0f1e31fdbc6e20f01ade4f685
Edmond <Edmond.Akpan@gmail.com>
2020-09-12 21:21:58 +0900

    reword comment

--- api/app/models/performance_review_feedback.rb
+++ api/app/models/performance_review_feedback.rb
@@ -2,7 +2,7 @@ class PerformanceReviewFeedback < ApplicationRecord
   belongs_to :performance_review
   belongs_to :employee
 
-  # convenience method to check pending status of a required performance review feedback
+  # helper method to check pending status of a required performance review feedback
   def pending?
     # nil content field used for logically pending review status
     content.nil?
