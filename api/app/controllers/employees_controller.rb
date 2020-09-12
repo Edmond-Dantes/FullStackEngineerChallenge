b3438cda747dade4db46472713c8c1f9fca60751
Edmond <Edmond.Akpan@gmail.com>
2020-09-12 18:42:11 +0900

    updates employee index placeholder implementation

--- api/app/controllers/employees_controller.rb
+++ api/app/controllers/employees_controller.rb
@@ -1,6 +1,10 @@
 class EmployeesController < ApplicationController
   def index
-    render json: {}
+    if employee = Employee.find_by(id: employee_id)
+      render json: employee
+    else
+      render json: nil, status: :not_found
+    end
   end
 
   private
